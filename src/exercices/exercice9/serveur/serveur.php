041354d5aab564e28a01f6549daeacc3f51d4005
tercier <truditercier@gmail.com>
2025-02-03 14:48:33 +0100

    ex9

--- src/exercices/exercice9/serveur/serveur.php
+++ src/exercices/exercice9/serveur/serveur.php
@@ -1,29 +1,43 @@
 <?php
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-	if($_POST['action'] == "connect")
-	{
-		// doit controller que le password donné (en post) est bien 'emf'
-		// si c'est ok => enregistrement de "emf" dans une variable de session et écrire '<result>true</result>'
-		// si c'est faux => effacer la variable de session et écrire '<result>false</result>'
-		echo '<result>false</result>';
-	}
+session_start(); // Démarre la session pour utiliser les variables de session
 
-	if($_POST['action'] == "disconnect")
-	{
-		// effacer la variable de session et écrire <result>true</result>
-		echo '<result>true</result>';
-	}
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if ($_POST['action'] == "connect") {
+        // Vérifier que le mot de passe est "emf"
+        if ($_POST['password'] == 'emf') {
+            $_SESSION['logged'] = 'emf';  // Enregistrer la connexion dans la session
+            echo '<result>true</result>';
+        } else {
+            // Si le mot de passe est incorrect, effacer la session et renvoyer false
+            session_unset();
+            echo '<result>false</result>';
+        }
+    }
+
+    if ($_POST['action'] == "disconnect") {
+        // Effacer la variable de session 'logged' et retourner true
+        session_unset();
+        echo '<result>true</result>';
+    }
+
 }
 
-if ($_SERVER['REQUEST_METHOD'] == 'GET'){
-	if($_GET['action'] == "getInfos")
-	{
-		// tout d'abord contrôler que la variable de session 'logged' contient bien "emf"
-		// si oui => retourner <users><user><name>Victor Legros</name><salaire>9876</salaire></user><user><name>Marinette Lachance</name><salaire>7540</salaire></user><user><name>Gustave Latuile</name><salaire>4369</salaire></user><user><name>Basile Ledisciple</name><salaire>2384</salaire></user></users>
-		// si non => écrire <message>DROITS INSUFFISANTS</message>
-		echo '<message>DROITS INSUFFISANTS</message>';
-	}
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    if ($_GET['action'] == "getInfos") {
+        // Vérifier si l'utilisateur est connecté en vérifiant la session
+        if (isset($_SESSION['logged']) && $_SESSION['logged'] == 'emf') {
+            // Si l'utilisateur est connecté, retourner les informations des utilisateurs
+            echo '<users>
+                    <user><name>Victor Legros</name><salaire>9876</salaire></user>
+                    <user><name>Marinette Lachance</name><salaire>7540</salaire></user>
+                    <user><name>Gustave Latuile</name><salaire>4369</salaire></user>
+                    <user><name>Basile Ledisciple</name><salaire>2384</salaire></user>
+                  </users>';
+        } else {
+            // Si l'utilisateur n'est pas connecté, retourner un message d'erreur
+            echo '<message>DROITS INSUFFISANTS</message>';
+        }
+    }
 }
-
 ?>
